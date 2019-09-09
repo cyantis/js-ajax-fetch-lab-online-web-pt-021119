032e94ec889affacafec75aff3cca3d9be2c0b8e
cyantis <cyantis@gmail.com>
2019-09-09 16:27:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const baseURL = 'https://api.github.com';
-const user = '<YOUR_USERNAME>';
+const user = 'cyantis';
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '';
+  return 'c92937c6d1b15f2d31c054ce7d84e91dff6d02e2';
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/js-ajax-fetch-lab';
+  console.log(`${baseURL}/repos/${repo}/forks`);
   //use fetch to fork it!
+  fetch(
+    `${baseURL}/repos/${repo}/forks`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`,
+      }
+    }
+  ).then(res => console.log(res));
 }
 
 function showResults(json) {
